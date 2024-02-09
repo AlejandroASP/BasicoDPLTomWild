d8fe3e213de181be03714c831b53e0546867a419
AlejandroASP <alejandroasp71@gmail.com>
2024-02-09 09:16:21 +0000

    war

---
+++ TareaTomWild_AlejandroSP/src/main/java/es/cifpcm/TareaTomWild_AlejandroSP/Controller/MainController.java
@@ -0,0 +1,12 @@
+package es.cifpcm.TareaTomWild_AlejandroSP.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
