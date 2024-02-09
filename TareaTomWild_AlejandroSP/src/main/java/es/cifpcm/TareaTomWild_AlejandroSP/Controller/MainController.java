176afb14b234d16f60ca5877f71ce9ee1dd0b077
AlejandroASP <alejandroasp71@gmail.com>
2024-02-09 08:31:02 +0000

    subiendo proyecto

---
+++ TareaTomWild_AlejandroSP/src/main/java/es/cifpcm/TareaTomWild_AlejandroSP/Controller/MainController.java
@@ -0,0 +1,13 @@
+package es.cifpcm.TareaTomWild_AlejandroSP.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public RedirectView index(){
+        return new RedirectView("index.hmtl");
+    }
+}
---
+++ TareaTomWild_AlejandroSP/src/test/java/es/cifpcm/TareaTomWild_AlejandroSP/TareaTomWildAlejandroSpApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cifpcm.TareaTomWild_AlejandroSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaTomWildAlejandroSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
